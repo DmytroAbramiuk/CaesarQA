3a876b43e51c892f97a45156e724e0b8d5000f0e
DmytroAbramiuk <128092807+DmytroAbramiuk@users.noreply.github.com>
2024-05-07 18:19:22 +0300

    main changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(Caesar.encrypt("hello", 1));
         System.out.println(Caesar.encrypt("hello", 2));
+        System.out.println(Caesar.encrypt("hello", 3));
     }
 }
