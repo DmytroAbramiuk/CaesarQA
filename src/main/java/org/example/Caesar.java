c1cb6cebbc839da1612b9b6ca7a2453b8383ddf3
DmytroAbramiuk <128092807+DmytroAbramiuk@users.noreply.github.com>
2024-05-07 17:33:10 +0300

    initial commit

---
+++ src/main/java/org/example/Caesar.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Caesar {
+    public static String encrypt(String text, int shift) {
+        return "encrypted";
+    }
+    public static String decrypt(String text, int shift) {
+        return "decrypted";
+    }
+}
